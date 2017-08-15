e00f59cd08517c3869251e8d96594102b28431a7
Innovationbham_2 <Innovationbham_2@UABiLab-2s-iMac.local>
2017-08-15 14:55:50 -0500

    app working

--- app.js
+++ app.js
@@ -1,32 +1,96 @@
 var app = angular.module('myApp',["ngRoute"]);
 app.config(function($routeProvider){
     $routeProvider
-    .when('/'),{
+    .when('/',{
         templateUrl: "../views/home.html"
-    }
-    .when('/products'),{
+    })
+    .when('/products',{
     templateUrl: "../views/products.html"
-}
-    .when('/miscellaneous'),{
+})
+    .when('/misc',{
     templateUrl: "../views/misc.html"
-}
-    .when('/invoices'),{
+})
+    .when('/invoices',{
     templateUrl: "../views/invoices.html"
-}
-    .when('/checkout'),{
+})
+    .when('/checkout',{
     templateUrl: "../views/checkout.html"
-}
-    .when('/products:id'),{
-    templateUrl: "../views/productinfo.html"
-}
-    .when('/miscellaneous:id'),{
-    templateUrl: "../views/miscinfo.html"
-}
+})
+//     .when('/products:id',{
+//     templateUrl: "../views/productinfo.html"
+// })
 });
+// //Get all products
 app.controller("GetProducts",function($scope,$http){
-    console('Get product initialized')
-    $http.get('http://iambham-store-dev.us-east-1.elasticbeanstalk.com/api/v1/invoices/all')
+    // console('Get product initialized')
+    $http.get('http://iambham-store-dev.us-east-1.elasticbeanstalk.com/api/v1/products/all?category=apparel',{
+       headers:{'Filter' : 'f0414a37-8143-11e7-8e40-12dbaf53d968'} 
+    })
         .then(function(response){
-            $scope.products=response.data
+            //console.log(response)
+            console.log('get request processed')
+            $scope.products = response.data.data
+            
+                console.log($scope.products)
+           
         })
     })
+app.controller("GetMisc",function($scope,$http){
+    // console('Get product initialized')
+    $http.get('http://iambham-store-dev.us-east-1.elasticbeanstalk.com/api/v1/products/all?category=misc',{
+       headers:{'Filter' : 'f0414a37-8143-11e7-8e40-12dbaf53d968'} 
+    })
+        .then(function(response){
+            //console.log(response)
+            console.log('get request processed')
+            $scope.products = response.data.data
+            
+                console.log($scope.products)
+           
+        })
+    })
+// //Get one products
+// app.controller("GetOneProducts",function($scope,$http,$routeParams){
+//     console('Get product initialized')
+//     var id=$routeParams.id;
+//     $http.get('http://iambham-store-dev.us-east-1.elasticbeanstalk.com/api/v1/products/one/'+id)
+//         .then(function(response){
+//             console.log('get request processed')
+//             $scope.products=response.data
+//         })
+//     })
+// //Get all invoices
+// app.controller("GetInvoices",function($scope,$http){
+//     console('Get product initialized')
+//     $http.get('http://iambham-store-dev.us-east-1.elasticbeanstalk.com/api/v1/invoices/all')
+//         .then(function(response){
+//             console.log('get request processed')
+//             $scope.invoices=response.data
+//         })
+//     })
+// //Get one invoice
+// app.controller("GetOneProducts",function($scope,$http,$routeParams){
+//     console('Get product initialized')
+//     var id=$routeParams.id;
+//     $http.get('http://iambham-store-dev.us-east-1.elasticbeanstalk.com/api/v1/invoices/one/'+id)
+//         .then(function(response){
+//             console.log('get request processed')
+//             $scope.invoices=response.data
+//         })
+//     })
+// //Post Invoice
+// app.controller("PostProduct",function($scope,$http){
+//     console('Get product initialized')
+//     $scope.postInvoice = function() {
+//         console.log('i clicked');
+//         var data= {
+//             date : $scope.date,
+//             price : $scope.price,
+//         };
+//         $http({ 
+//             method: 'POST', 
+//             url: 'http://iambham-store-dev.us-east-1.elasticbeanstalk.com/api/v1/invoices', 
+//             data: data 
+//         })
+//     }
+// })
