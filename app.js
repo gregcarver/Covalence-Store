3e0fe580ec541a8009df2153ec14c80df83de0fb
Innovationbham_2 <Innovationbham_2@UABiLab-2s-iMac.local>
2017-08-15 11:17:07 -0500

    six

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
 var app = angular.module('myApp',["ngRoute"]);
-app.config(function($routeProvider))
+app.config(function($routeProvider){
+    $routeProvider
+    .when('/'),{
+        templateUrl: "../views/home.html"
+    }
+    .when('/products'),{
+    templateUrl: "../views/products.html"
+}
+    .when('/miscellaneous'),{
+    templateUrl: "../views/misc.html"
+}
+    .when('/invoices'),{
+    templateUrl: "../views/invoices.html"
+}
+    .when('/checkout'),{
+    templateUrl: "../views/checkout.html"
+}
+    .when('/products:id'),{
+    templateUrl: "../views/productinfo.html"
+}
+    .when('/miscellaneous:id'),{
+    templateUrl: "../views/miscinfo.html"
+}
+});
+app.controller("GetProducts",function($scope,$http){
+    console('Get product initialized')
+    $http.get('http://iambham-store-dev.us-east-1.elasticbeanstalk.com/api/v1/invoices/all')
+        .then(function(response){
+            $scope.products=response.data
+        })
+    })
